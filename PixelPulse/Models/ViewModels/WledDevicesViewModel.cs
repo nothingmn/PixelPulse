cdcdd9ec3c7df9623ffc94abe9b5c66773de16af
Rob Chartier <rob.chartier@ethersolutions.ca>
2024-03-15 15:52:16 -0700

    clear add new input after added

--- PixelPulse/Models/ViewModels/WledDevicesViewModel.cs
+++ PixelPulse/Models/ViewModels/WledDevicesViewModel.cs
@@ -38,6 +38,7 @@ public partial class WledDevicesViewModel : ObservableObject
     private async Task AddDevice()
     {
         await _deviceManager.AddDeviceByHostOrIp(AddNewHost);
+        AddNewHost = "";
     }
 
     [RelayCommand]
